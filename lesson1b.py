914ddd76b3819b5bf11271f6f3937ebb12d49048
lawrencegitonga310-jpg <lawrencegitonga310@gmail.com>
2026-02-05 11:18:20 -1200

    added lesson1b

---
+++ lesson1b.py
@@ -0,0 +1,17 @@
+# python Data types
+# string- it is a series of characters that are inside of quotes
+name="lawrence"
+print(name)
+print(type(name))
+
+# numeric
+# int - this indicates a whole number in python . for an intager, we dont use quotes
+
+age=39
+print("the age of a person is:",age)
+print(type(age))
+
+# float: a number with decimal
+coordinates=15.45
+print("the coordinate area",coordinates)
+print(type(coordinates))
