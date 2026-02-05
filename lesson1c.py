5ae5e0c957f065ba8556ba8a55dc0af9c38026e8
lawrencegitonga310-jpg <lawrencegitonga310@gmail.com>
2026-02-05 11:55:34 -1200

    added lesson1c

---
+++ lesson1c.py
@@ -0,0 +1,38 @@
+# arthmetic operations
+# addition
+number1 = 10
+number2 = 20
+addition = number1 + number2
+print("the sum of the two numbers is",addition) 
+
+# substruction
+number3=50
+number4=15
+difference=number3 - number4
+print("the difference of numbers is",difference)
+
+# multiplication
+number5=10
+number6=6
+product= number5 * number6
+print("the productof the numbers is",product)
+
+# division
+# number7 and number8
+
+# modulus - this means the remainder of a number when get subdivided by another one
+
+number9=14
+number10=5
+modulus = number9 % number10
+print("the modulus of the numbers is:",modulus)
+
+# floor division - this is the number of times a number when divides another ignoring the remeider
+floor = number9 //number10
+print("the floor division of the two numbers is",floor)
+
+# Exponent(**)
+# The power of a number
+number11=10
+exponent = number11 ** 2
+print("the exponent of the number is",exponent)
