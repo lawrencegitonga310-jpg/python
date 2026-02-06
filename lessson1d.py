58d09d4e508bc415c0bdea76cc084026825e73e2
lawrencegitonga310-jpg <lawrencegitonga310@gmail.com>
2026-02-05 12:13:41 -1200

    added lesson1d

---
+++ lessson1d.py
@@ -0,0 +1,12 @@
+# create a program that is able to calculate the simple interest given the principle as 50000, rate as 5% and time as 8 years.SI =(P * R * T)/100
+principle=50000
+rate=5
+time= 8
+simpleinterest=(principle * rate *time) /100
+print("the simpleinterest is",simpleinterest)
+
+# write a python that is able to calculate the BMI of a person whose weight is 78kgs and height is 1.75 meters.BMI=(weight)/ (height squired)
+
+# find the area and perimeter of a rectangle whose length is 48cm and width 25cm 
+
+# research on python list tuple and dictonary data types 
